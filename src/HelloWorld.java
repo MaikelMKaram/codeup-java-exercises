e984b4a0f911dc8a25c2ce510d7587b396ce55f4
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-07 23:40:51 -0600

    Java exercises progress save

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,24 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.print("Hello, ");
-        System.out.print("big G!");
+        System.out.println("big G!");
+        int myFavoriteNumber = 9;
+        System.out.println(myFavoriteNumber);
+        String myString = "What would you dooooOOOOOooooo... for a Clondyke Bar?";
+        System.out.println(myString);
+//        char notMyString = 'A';
+//        System.out.println(notMyString);
+//        String notNotMyString = 3.14159;
+//        System.out.println(notNotMyString);
+        long myNumber = 123;
+        System.out.println(myNumber);
+        float notMyNumber = 3.14F;
+        System.out.println(notMyNumber);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
     }
 }
