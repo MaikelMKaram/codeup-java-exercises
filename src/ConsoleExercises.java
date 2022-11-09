8b3387da5f101d2ceceb0e36a8b75dc6b73b880e
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-09 10:46:36 -0600

    Control statements and Loops exercise start

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -22,10 +22,22 @@ public class ConsoleExercises {
 //        String userInput = scanner.nextLine();
 //        System.out.println("You entered: --> \"" + userInput + "\" <--");
 
-        System.out.print("Enter a values for the length and width of our CodeUp classroom respectively: ");
-        float userLength = scanner.nextFloat();
-        float userWidth = scanner.nextFloat();
-        float areaOfRoom = ((userWidth + userLength) * 2);
-        System.out.println("Area of the room is: --> \"" + areaOfRoom + "\" <--");
+//        System.out.print("Enter a values for the length and width of our CodeUp classroom respectively: ");
+//        float userLength = scanner.nextFloat();
+//        float userWidth = scanner.nextFloat();
+//        float perimeterOfRoom = ((userWidth + userLength) * 2);
+//        float areaOfRoom = (userWidth * userLength);
+//        System.out.println("The area of the room is: " + areaOfRoom + ". The perimeter of the room is: " + perimeterOfRoom + ".");
+
+
+//Lecture exercise
+//ToDo: save your age to a variable. create a scanner class. have the user guess your age. return whether or not the guessed age is the same as the age saved: int age = 32;
+
+//int myAge = 32;
+//
+//        System.out.print("Guess my age: ");
+//        int inputAge = scanner.nextInt();
+//        System.out.println("You entered: --> \"" + inputAge + "\" <--");
+//        System.out.print(inputAge == myAge);
     }
 }
