3c4ca118602e295891e216291e2ffceefb3efb7b
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-15 14:31:50 -0600

    OOP exercise 2 progress

--- src/util/Input.java
+++ src/util/Input.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args){
         Input myInput = new Input(new Scanner(System.in));
-        System.out.println("myInput.getString() = " + myInput.getString());
-        System.out.println("myInput.yesNo() = " + myInput.yesNo());
-
+//        System.out.println("myInput.getString() = " + myInput.getString());
+//        System.out.println("myInput.yesNo() = " + myInput.yesNo());
+        System.out.println("the number " + myInput.getInt(2,8) + " is within the range I was thinking of");
     }
 
     //Private scanner property/field
@@ -30,7 +30,12 @@ public class Input {
     }
 
     public int getInt(int min, int max){
-
+        System.out.println("\nI'm thinking of a range of numbers. Please guess a number within that range");
+        int num = scanner.nextInt();
+        if (num < min || num > max){
+            return getInt(min, max);
+        } else {
+            return num;
+        }
     }
-
 }
