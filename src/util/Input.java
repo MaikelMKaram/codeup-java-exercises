b0868ed09da1b7f060fadc5cc72f0f72f5834e92
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-15 12:16:46 -0600

    OOP exercise 2 progress

--- src/util/Input.java
+++ src/util/Input.java
@@ -4,8 +4,28 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args){
+        Input myInput = new Input(new Scanner(System.in));
+        System.out.println("myInput.getString() = " + myInput.getString());
+        System.out.println("myInput.yesNo() = " + myInput.yesNo());
+
     }
 
-    private Scanner scanner = new Scanner(System.in);
+    //Private scanner property/field
+    private Scanner scanner;
 
+    //Constructor
+    public Input(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    //methods
+    public String getString(){
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        System.out.println("\n[Yes/No]");
+        String nextLine = this.scanner.nextLine();
+        return (nextLine.equalsIgnoreCase("y") || nextLine.equalsIgnoreCase("yes") || nextLine.equalsIgnoreCase("true"));
+    }
 }
