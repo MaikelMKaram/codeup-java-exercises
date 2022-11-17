2f7a7867d2129aa7ddedd4a13f36a43498686b53
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-17 15:29:42 -0600

    movie exercise completed

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -7,22 +7,26 @@ import java.util.Scanner;
 public class MoviesApplication {
 
     public static void main(String[] args) {
-        //Give the user a list of options for viewing all the movies [completed in line 10] or viewing movies by category.
+        choiceList();
+    }
+
+    public static void choiceList(){
+
+        //Give the user a list of options for viewing all the movies or viewing movies by category.
         Movie[] movArray = MoviesArray.findAll();
 
-        //userInput call
-        System.out.println("Pick a movie category: ");
-        System.out.println("0 - exit");
-        System.out.println("1 - view all movies");
-        System.out.println("2 - view movies in the animated category");
-        System.out.println("3 - view movies in the drama category");
-        System.out.println("4 - view movies in the horror category");
-        System.out.println("5 - view movies in the scifi category");
-        System.out.println("6 - view movies in the musical category");
-        System.out.println("7 - view movies in the comedy category");
+        // userInput call
+        System.out.print("Pick a movie category: \n" +
+                "0 - exit \n" +
+                "1 - view all movies \n" +
+                "2 - view movies in the animated category\n" +
+                "3 - view movies in the drama category\n" +
+                "4 - view movies in the horror category\n" +
+                "5 - view movies in the scifi category\n" +
+                "6 - view movies in the musical category\n" +
+                "7 - view movies in the comedy category\n");
 
-
-        Input  myInput = new Input(new Scanner(System.in));
+        Input myInput = new Input(new Scanner(System.in));
         int userChoice = myInput.getInt();
 
         for (int i = 0; i < movArray.length; i++) {
@@ -43,6 +47,9 @@ public class MoviesApplication {
             } else if (userChoice == 7 && movArray[i].getCategory().equalsIgnoreCase("comedy")){
                 System.out.println(movArray[i].getName() + " | " + movArray[i].getCategory());
             }
+        } if (userChoice == 0){
+        } else {
+            choiceList();
         }
     }
 }
