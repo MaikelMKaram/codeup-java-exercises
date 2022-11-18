3c4d545697614d3c79e94c9c5a2dee98ae7819ce
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-18 16:50:00 -0600

    grades exercise started

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -7,6 +7,9 @@ public class ShapesTest {
 
         System.out.println("box1.getArea() = " + box1.getArea());
         System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
+        Rectangle box2 = new Square(5);
+        System.out.println("box2.getArea() = " + box2.getArea());
+        System.out.println("box2.getPerimeter() = " + box2.getPerimeter());
     }
 
 }
