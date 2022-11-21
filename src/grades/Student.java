0342959b3706b9e4b62fbe73c0b65a9e77a04a97
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-21 10:21:14 -0600

    grades exercise 1 completed

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -4,8 +4,32 @@ import java.util.ArrayList;
 
 public class Student {
     private String name;
-    private ArrayList<integer> grades = new ArrayList<integer>();
+    private ArrayList<Integer> grades;
 
+    public static void main(String[] args) {
+        Student Student1 = new Student("Student1", 90);
+        Student1.addGrade(70);
+        System.out.println("Student1.getGradeAverage() = " + Student1.getGradeAverage());
+    }
 
+    public Student(String name, int grades){
+        this.name = name;
+        this.grades = new ArrayList<>();
+        this.grades.add(grades);
+    }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        int total = 0;
+        for(int i = 0; i < grades.size(); i++){
+            total += grades.get(i);
+        } return total/grades.size();
+    }
 }
