8274ac1233a3450f81d781d40d29497bc7e4d8ac
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-21 15:13:28 -0600

    GradesApp progress save

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -3,23 +3,34 @@ package grades;
 import java.util.HashMap;
 
 public class GradesApplication {
-    public static void getGrades(){
+
+    public static void main(String[] args) {
         HashMap<String, Student> students = new HashMap<>();
-        Student students1 = new Student("Jack");
-        Student students2 = new Student("Jim");
-        Student students3 = new Student("James");
-        Student students4 = new Student("John");
-        students1.addGrade(100);
-        students1.addGrade(90);
-        students1.addGrade(90);
-        students2.addGrade(90);
-        students2.addGrade(80);
-        students2.addGrade(80);
-        students3.addGrade(80);
-        students3.addGrade(70);
-        students3.addGrade(70);
-        students4.addGrade(70);
-        students4.addGrade(60);
-        students4.addGrade(60);
+        Student student1 = new Student("Jack");
+        student1.addGrade(100);
+        student1.addGrade(90);
+        student1.addGrade(90);
+
+        Student student2 = new Student("Jim");
+        student2.addGrade(90);
+        student2.addGrade(80);
+        student2.addGrade(80);
+
+        Student student3 = new Student("James");
+        student3.addGrade(80);
+        student3.addGrade(70);
+        student3.addGrade(70);
+
+        Student student4 = new Student("John");
+        student4.addGrade(70);
+        student4.addGrade(60);
+        student4.addGrade(60);
+
+        students.put("Jack_n_Coke",student1);
+        students.put("Jim_Bean",student2);
+        students.put("James_on_Ice",student3);
+        students.put("Johnny",student4);
+
+
     }
 }
