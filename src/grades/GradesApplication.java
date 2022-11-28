00b37451ed8d708e3da1f0147a0fcbc3c49e1de5
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-28 14:24:15 -0600

    grades progress update

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -21,15 +21,6 @@ public class GradesApplication {
         student2.addGrade(80);
         student2.addGrade(80);
 
-        Student student3 = new Student("James");
-        student3.addGrade(80);
-        student3.addGrade(70);
-        student3.addGrade(70);
-
-        Student student4 = new Student("John");
-        student4.addGrade(70);
-        student4.addGrade(60);
-        student4.addGrade(60);
 
         students.put("Jack_n_Coke",student1);
         students.put("Jim_Beam",student2);
