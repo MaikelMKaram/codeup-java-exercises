3d60c87192a4834f3889528b675401a6571f9ce9
Maikel Karam <maikel.m.karam@gmail.com>
2023-01-03 09:13:34 -0600

    hw exercises added/corrected

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,4 +1,25 @@
 package shapes;
 
-public class Quadrilateral {
+abstract class Quadrilateral extends Shape implements Measureable{
+
+    protected int length;
+
+    protected int width;
+
+    public Quadrilateral(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    abstract public void setLength(int length);
+
+    abstract public void setWidth(int width);
 }
